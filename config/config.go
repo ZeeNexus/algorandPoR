2f6000274f2054947e3b3ca28cc0b15b27a23c59
xddlg <carlosdiazdeleon@my.unt.edu>
2019-11-23 13:03:42 -0600

    making the def init rep 100000

--- config/config.go
+++ config/config.go
@@ -31,6 +31,8 @@ import (
 	"github.com/algorand/go-algorand/util/codecs"
 )
 
+const RepDefaultInitVal = 100000
+
 // Devnet identifies the 'development network' use for development and not generally accessible publicly
 const Devnet protocol.NetworkID = "devnet"
 
